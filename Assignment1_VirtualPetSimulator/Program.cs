97e53a168863ee3109da81990fb6f3f5eb321ef9
Sai Sravan Kumar Repaka <Srepaka0820@conestogac.on.ca>
2024-02-03 12:05:31 -0500

    Add project files.

---
+++ Assignment1_VirtualPetSimulator/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VirtualPetSimulator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Choosing pet type
+
+            Console.WriteLine("Please choose a type of pet");
+            Console.WriteLine("1. Cat");
+            Console.WriteLine("2. Dog");
+            Console.WriteLine("3. Rabbit");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("User input is : \n");
+
+            int PetTypeSel = Convert.ToInt32(Console.ReadLine());
+
+
+
+            switch (PetTypeSel)
+
+            {
+
+                case 1:
+
+                    Console.WriteLine("\nYou've chosen a Cat. What would you like to name your Cat?");
+
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("\nYou've chosen a Dog. What would you like to name your Dog?");
+
+                    break;
+
+                case 3:
+
+                    Console.WriteLine("\nYou've chosen a Rabbit. What would you like to name your Rabbit?");
+
+                    break;
+
+                default:
+
+                    Console.WriteLine("Please enter the input as 1 or 2 or 3 only.");
+
+                    break;
+
+            }
+
+
+            Console.WriteLine("\nUser input is : \n");
+
+            String PetName = Console.ReadLine();
+
+            VirtualPet PetDet = new VirtualPet(PetName);
+
+            Console.WriteLine("\nWelcome, " + PetName + "!. Lets take good care of you.");
+
+
+
+            bool UserPetFunctionSelection = true;
+
+            while (UserPetFunctionSelection)
+            {
+
+                Console.WriteLine("\nMain Menu \n");
+
+                Console.WriteLine("1. Feed " + PetName);
+                Console.WriteLine("2. Play with " + PetName);
+                Console.WriteLine("3. Let " + PetName + " rest");
+                Console.WriteLine("4. Check " + PetName + "'s status");
+                Console.WriteLine("5. Exit");
+
+
+                Console.WriteLine("User input is : \n");
+                int MainMenuSel = Convert.ToInt32(Console.ReadLine());
+
+                switch (MainMenuSel)
+
+                {
+
+                    case 1:
+
+                        PetDet.FeedPet();
+
+
+                        break;
+
+                    case 2:
+
+                        PetDet.PlayWithPet();
+
+                        break;
+
+                    case 3:
+
+                        PetDet.RestThePet();
+
+                        break;
+
+                    case 4:
+
+                        PetDet.DefaultVal();
+
+                        break;
+
+                    case 5:
+                        UserPetFunctionSelection = false;
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+
+
+
+            }
+
+
+        }
+
+    }
+
+}
---
+++ Assignment1_VirtualPetSimulator/VirtualPet.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetSimulator
+{
+    public class VirtualPet
+    {
+        // Properties
+        public string Name { get; set; }
+        public int Hunger { get; set; }
+        public int Happiness { get; set; }
+        public int Health { get; set; }
+
+
+        // Constructors
+
+        public VirtualPet(string PetName)
+        {
+            Name = PetName;
+            Hunger = 5;
+            Happiness = 5;
+            Health = 5;
+
+        }
+
+        public void FeedPet()
+        {
+            Hunger = --Hunger;
+            Health = ++Health;
+            Console.WriteLine($"You fed  {Name}.His Hunger decreases, and Health increases.");
+        }
+
+        public void PlayWithPet()
+        {
+            Hunger = ++Hunger;
+            Happiness = ++Happiness;
+            Console.WriteLine($"You played with {Name}.His Hunger increases, and happiness increases.");
+
+        }
+
+        public void RestThePet()
+        {
+            Happiness = --Happiness;
+            Health = ++Health;
+            Console.WriteLine($"You let the  {Name} rest.His Health increases and happiness decreases.");
+
+        }
+
+        public void DefaultVal()
+        {
+
+
+            Console.WriteLine($"Hunger: {Hunger}");
+            Console.WriteLine($"Happiness: {Happiness}");
+            Console.WriteLine($"Health: {Health}");
+            Console.WriteLine();
+        }
+
+
+
+    }
+
+}
